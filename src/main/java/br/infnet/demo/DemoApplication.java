b0b0d0e8ab652996f9371cae5675694629e4e68a
Oliv4 <oliva.git@hotmail.com>
2020-11-20 18:22:00 -0300

    Update DemoApplication.java

--- src/main/java/br/infnet/demo/DemoApplication.java
+++ src/main/java/br/infnet/demo/DemoApplication.java
@@ -10,4 +10,8 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
 }
