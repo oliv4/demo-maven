8311a4a4ae29171214da12e14485cace933aefb5
oliv4 <oliva.git@hotmail.com>
2020-11-20 19:16:53 -0300

    tests

--- src/test/java/br/infnet/demo/DemoApplicationTests.java
+++ src/test/java/br/infnet/demo/DemoApplicationTests.java
@@ -1,13 +1,32 @@
 package br.infnet.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DemoApplicationTests {
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-	@Test
-	void contextLoads() {
+@SpringBootTest
+public class DemoApplicationTests {
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
 	}
+
+    @Test
+    public void shouldAnswerWithFalse()
+    {
+        assertFalse( false );
+	}
+
+    @Test
+    public void shouldAnswerWithString()
+    {
+        assertEquals( "Hello World!", "Hello World!" );
+    }
 
 }
